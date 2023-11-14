0c6b79eaa2f25a021b94bc9900c08972c8e55840
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-11-15 00:09:13 +0300

    Create 4.cpp

---
+++ ETS1303_15_TESFAMICHAEL_TAFERE/4.cpp
@@ -0,0 +1,31 @@
+//A program that calculates x^y where x and y are provided by the user.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x, y;
+
+    cout << "Enter the value of X: ";
+    cin >> x;
+
+    cout << "Enter the value of Y: ";
+    cin >> y;
+
+    double result = pow(x, y);
+
+    cout << "The result of the expression " << x << "^" << y << " is: " << result <<endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
