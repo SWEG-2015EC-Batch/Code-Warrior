f551f36a807cd02733cce6af929b88dd3ded9794
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2023-11-14 15:44:33 -0500

    Create question1.cpp

---
+++ ETS1351_15_Yabets_Maregn/question1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+     //in this code bmi stands for body mass index
+    float weight, height, bmi;
+    cout<<"enter weight in kilogram:- ";
+    cin>>weight;
+    cout<<"enter height in meter:- ";
+    cin>>height;
+    //bmi can be calculted by dividing the weight in kilogram by height squred in meter. 
+    bmi = weight/(height*height);
+    cout<<"Body Mass Index = "<<bmi <<" kg/m^2";
+    return 0;
+}
