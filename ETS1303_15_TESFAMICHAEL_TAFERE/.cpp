72a274a23db0f0e9a224abfe460928df47b8a27e
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-11-14 22:59:30 +0300

    Create .cpp

---
+++ ETS1303_15_TESFAMICHAEL_TAFERE/.cpp
@@ -0,0 +1,24 @@
+//BMI calculator
+#include <iostream>
+using namespace std;
+
+int main() {
+    float height, weight, bmi;
+
+    cout << "Enter your height in meters: ";
+    cin >> height;
+
+    cout << "Enter your weight in kilograms: ";
+    cin >> weight;
+
+    bmi = weight / (height * height);
+
+    cout << "Your Body Mass Index is: " << bmi << endl;
+
+    return 0;
+}
+
+
+
+
+
