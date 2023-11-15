694b99f8abe4446c60ee122897f545c03dfcfa0e
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2023-11-15 08:53:15 +0300

    Update Question1.cpp

--- ETS1219_SEID_HULLE/Question1.cpp
+++ ETS1219_SEID_HULLE/Question1.cpp
@@ -1,1 +1,19 @@
+//BMI calculator
+#include <iostream>
+using namespace std;
 
+int main() {
+    float height, weight, bmi;
+
+    cout << "Enter your height in meters: ";
+    cin >> height;
+
+    cout << "Enter your weight in kilograms: ";
+    cin >> weight;
+
+    bmi = weight / (height * height);
+
+    cout << "Your Body Mass Index is: " << bmi << endl;
+
+    return 0;
+}
