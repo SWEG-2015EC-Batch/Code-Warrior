a640467807ad1c4bfc4708232f1ef7585624a799
wendmagegn <149209819+wende12github@users.noreply.github.com>
2023-11-15 12:17:11 +0300

    Update Code4.cpp

--- ETS1343_15_WENDMAGEGN_TAJURA/Code4.cpp
+++ ETS1343_15_WENDMAGEGN_TAJURA/Code4.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+int main(){
+float x, y, result;
+       
+        cout <<"Enter the x value ";
+        cin >>x;
+        cout <<"Enter the y value ";
+        cin >>y;
+        result = pow(x, y);
+        cout <<"The  result of the expression is: \n" <<x <<"^" <<y << " = " <<result;
 
+return 0;
+}
