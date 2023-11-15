9d020e9d1bc03b4407957f7fdc47f36aa9ce500a
wendmagegn <149209819+wende12github@users.noreply.github.com>
2023-11-15 11:55:58 +0300

    Update Code3.cpp

--- ETS1343_15_WENDMAGEGN_TAJURA/Code3.cpp
+++ ETS1343_15_WENDMAGEGN_TAJURA/Code3.cpp
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <iostream>
+using namespace std;
+int main(){
+  
+   char letter;
+        // Prompt for input
+        cout <<"Please enter the letter by using your keyboard ";
+        // Receive Input
+        cin >>letter;
+        // Convert to Uppercase or Lowercase
+        if (isalpha(letter)) {
+            char uppercaseLetter = toupper(letter);
+            char lowercaseLetter = tolower(letter);
+            // Output Result
+            cout <<"Upper letter: " <<uppercaseLetter <<"\n";
+            cout <<"Lower letter: " <<lowercaseLetter <<"\n";
 
+        } else {
+            cout <<" Invalid input, Please enter the letter by using your keyboard ";
+        }
+return 0;
+}
