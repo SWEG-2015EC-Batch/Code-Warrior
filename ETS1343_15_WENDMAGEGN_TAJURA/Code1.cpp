775ae0536562b034c0f5de6c40deb3a1102610c4
wendmagegn <149209819+wende12github@users.noreply.github.com>
2023-11-15 10:44:46 +0300

    Update Code1.cpp

--- ETS1343_15_WENDMAGEGN_TAJURA/Code1.cpp
+++ ETS1343_15_WENDMAGEGN_TAJURA/Code1.cpp
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+int main(){
+    float wight, hight, BMI;
+        cout <<"Please enter the wight of a peson ";
+        cin >>wight;
+        cout <<"Please enter the hight of a person ";
+        cin >>hight;
 
+        BMI = (wight)/(hight*hight);
+        cout << "The BMI of the person is: " <<BMI <<" Kg/m^2";
+return 0;
+}
