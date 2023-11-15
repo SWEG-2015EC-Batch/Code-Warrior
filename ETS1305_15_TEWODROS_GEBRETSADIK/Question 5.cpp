d2d53d00935dcce401562c57f7faf1e33cf85dfd
Tewodros Gebretsadik <149199747+Tewodros7@users.noreply.github.com>
2023-11-15 09:47:54 +0300

    Update Question 5.cpp

--- ETS1305_15_TEWODROS_GEBRETSADIK/Question 5.cpp
+++ ETS1305_15_TEWODROS_GEBRETSADIK/Question 5.cpp
@@ -1,9 +1,8 @@
-// using c++ to calculate employer of salary 
 #include <iostream>
 #include <string>
 using namespace std;
 int main() {
-    std::string employee_name;
+    string employee_name;
     int weekly_working_hours;
     double bonus_rate_per_hour, base_salary, gross_salary, net_salary, bonus_payment, pension_amount, tax_amount;
 cout << "Enter employee name: ";
@@ -20,10 +19,10 @@ cin >> base_salary;
     net_salary = gross_salary - (pension_amount + tax_amount);
     bonus_payment = weekly_working_hours * bonus_rate_per_hour;
 
-    std::cout << "Employee Name: " << employee_name <<endl;
-    std::cout << "Gross Salary: " << gross_salary <<endl;
-    std::cout << "Net Salary: " << net_salary <<endl;
-    std::cout << "Bonus Payment: " << bonus_payment <<endl;
+    cout << "Employee Name: " << employee_name <<endl;
+    cout << "Gross Salary: " << gross_salary <<endl;
+    cout << "Net Salary: " << net_salary <<endl;
+    cout << "Bonus Payment: " << bonus_payment <<endl;
 
     return 0;
 }
