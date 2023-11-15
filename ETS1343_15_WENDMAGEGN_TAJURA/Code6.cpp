8c7c14f966745dc8bc642b2aa0d7885e6a24d179
wendmagegn <149209819+wende12github@users.noreply.github.com>
2023-11-15 12:23:45 +0300

    Update Code6.cpp

--- ETS1343_15_WENDMAGEGN_TAJURA/Code6.cpp
+++ ETS1343_15_WENDMAGEGN_TAJURA/Code6.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+int main(){
+const int transmission_speed = 960;
+        int file_size;
+        cout << "please enterbthe size of the file to be sent in bytes: ";
+        cin >>file_size;
+        //calculate transmission time
+        double time_taken = static_cast<double>(file_size)/(transmission_speed);
+        cout << "Estimated time to send the file: " <<time_taken << " seconds" <<endl;
+
+
+        return 0;
+}
