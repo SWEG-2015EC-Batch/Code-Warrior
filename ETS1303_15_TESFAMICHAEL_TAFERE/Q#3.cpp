c1f5e9ad7d04e60267a6d53f4504036390097843
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-11-18 01:14:38 +0300

    Create Q#3.cpp

--- ETS1303_15_TESFAMICHAEL_TAFERE/Q#3.cpp
+++ ETS1303_15_TESFAMICHAEL_TAFERE/Q#3.cpp
@@ -1,4 +1,4 @@
-//While loop demo on a program that convert any letter to its uppercase or lowercase.
+// While loop demo on a program that convert any letter to its uppercase or lowercase.
 #include <iostream>
 #include <cctype>
 using namespace std;
