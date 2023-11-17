978e764330af9bdee4acb0bd84f9b0a215e98ed5
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2023-11-17 11:14:09 -0500

    Update question3.cpp

--- ETS1351_15_Yabets_Maregn/question3.cpp
+++ ETS1351_15_Yabets_Maregn/question3.cpp
@@ -2,11 +2,23 @@
 #include <ctype.h>
 using namespace std;
 int main(){
-    char letter, upperCaseLetter, lowerCaseLetter;
+    char letter;
+    int letterCase ;
     cout<<"enter letter:- ";
     cin>>letter; 
-    upperCaseLetter = toupper(letter);
-    lowerCaseLetter = tolower(letter);
-    cout<<"the upper and lower case of letter "<<letter<<" repectively is  "<<upperCaseLetter<<" and "<<lowerCaseLetter<<".";
+     cout<<"do u want to change it to uppercase or lowercase, type 1 for uppercase and 2 for lower case:- ";
+      cin>>letterCase;
+      if (letterCase==1){
+             letter = toupper(letter);
+        cout<<"the uppercase of the letter you entered is "<<letter;
+      }
+        
+    else if(letterCase==2){
+        letter = tolower(letter);
+    cout<<"the lowercase of the letter you entered is "<<letter;
+    }
+    else{
+        cout<<"make sure you entered single letter and you  chose between 1 and 2. ";
+    }
     return 0;
 }
