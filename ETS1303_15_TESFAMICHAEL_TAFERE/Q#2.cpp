e1e75571eae116f77f97b0bfb4c367fdfe515b41
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-11-17 20:40:11 +0300

    Update Q#2.cpp

--- ETS1303_15_TESFAMICHAEL_TAFERE/Q#2.cpp
+++ ETS1303_15_TESFAMICHAEL_TAFERE/Q#2.cpp
@@ -1,21 +1,21 @@
 //fuel tank mile analizer 
 #include <iostream>
-
 using namespace std;
-
-int main() 
+int main()
 {
-    double fuel_capacity, miles_per_gallon;
-
-    cout << "Enter the fuel capacity of the automobile in gallons: ";
-    cin >> fuel_capacity;
-
-    cout << "Enter the miles per gallon of the automobile: ";
-    cin >> miles_per_gallon;
-
-    double miles_without_refueling = fuel_capacity * miles_per_gallon;
-
-    cout << "The number of miles the automobile can be driven without refueling is: " << miles_without_refueling << endl;
-
-    return 0;
-}
+    float fule_capacity, mile_per_gallon, miles_without_refuleing;
+    
+    
+    cout<<"Enter fule capacity of the fule tank: ";
+    cin>>fule_capacity;
+    cout<<"Enter the mile per gallon of the automotive: ";
+    cin>>mile_per_gallon;
+    
+    
+   miles_without_refuleing = fule_capacity * mile_per_gallon;
+   
+   cout<<"The number of miles the automotive can be driven is "<< miles_without_refuleing<<" miles."
+;
+return 0;
+}    
+    
