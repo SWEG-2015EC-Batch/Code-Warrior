13d18b099878d2309ddafdb9d0f87ffb94d30dbc
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-11-18 01:05:15 +0300

    Update Q#3 using while loop😎😮‍💨.cpp

--- ETS1303_15_TESFAMICHAEL_TAFERE/Q#3.cpp
+++ ETS1303_15_TESFAMICHAEL_TAFERE/Q#3.cpp
@@ -1,29 +1,42 @@
-//A program that convert an entered letter to upper or lower case letters
+//while loop demo on a program that convert any letter to its uppercase or lowercase.
 #include <iostream>
 #include <cctype>
 using namespace std;
-
-int main() {
-    char letter;
+int main()
+{
+    char letter, Letter;
     int choice;
-
-    cout << "Enter a letter: ";
-    cin >> letter;
-
-    cout << "Choose 1 for uppercase or 2 for lowercase: ";
-    cin >> choice;
-
-    if (choice == 1) {
-        letter = toupper(letter);
-        cout << "The uppercase equivalent of " << letter << " is " << letter << "." <<endl;
-    }
-    else if (choice == 2) {
-        letter = tolower(letter);
-        cout << "The lowercase equivalent of " << letter << " is " << letter << "." <<endl;
-    }
-    else {
-        cout << "Invalid choice. Please choose either 1 or 2." <<endl;
-    }
-
-    return 0;
-}
+    
+    while(true)
+    {
+        
+        cout<<"Enter a letter: ";
+        cin>>letter;
+        
+        while(true)
+       {
+            cout<<"Choose 1 for uppercase or 2 for lowercase: ";
+            cin>>choice;
+            if((choice==1) || (choice==2))
+            break;
+       
+                 else
+        
+           {cout<<"Invalid choice. Please enter 1 or 2.\n";
+            }
+        }
+            if(choice==1)
+            {
+                Letter = toupper(letter);
+                cout<<"The uppercase equivalent to " <<letter<<" is "<<Letter<<".";
+            }
+            else if(choice==2)
+            {
+                Letter = tolower(letter);
+                cout<<"The lowercase equivalent of "<<letter<<" is "<<Letter<<".";
+            }break;
+   }
+   
+return 0;
+}             
+        
