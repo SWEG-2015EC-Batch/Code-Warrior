a642d4eb3a2b0ac61f7cbe738802f1769bbfef21
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2023-11-18 11:53:14 +0300

    Update Question4 using for loop.cpp

--- ETS1219_SEID_HULLE/Question4 using for loop.cpp
+++ ETS1219_SEID_HULLE/Question4 using for loop.cpp
@@ -1,1 +1,20 @@
+//A program that calculates x^y where x and y are provided by the user.
+#include <iostream>
+using namespace std;
 
+int main() {
+    int x, y, ans = 1; // Initialize ans to 1 to handle the case when y is 0
+
+    cout << "Enter the value of x: ";
+    cin >> x;
+    cout << "Enter the value of y: ";
+    cin >> y;
+
+    for (int i = 1; i <= y; ++i) {
+        ans *= x;
+    }
+
+    cout << "x^y = " << ans;
+
+    return 0;
+}
