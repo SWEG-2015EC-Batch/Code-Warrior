8e5318065f07623486b5e82067acc12a289d628c
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-11-18 05:38:58 +0300

    Create Q5#.cpp

--- ETS1303_15_TESFAMICHAEL_TAFERE/Q5#.cpp
+++ ETS1303_15_TESFAMICHAEL_TAFERE/Q5#.cpp
@@ -1,40 +1,38 @@
 
+//A program that calcutedthe gross salary , net salary and bonus payment.
 #include <iostream>
-#include <iomanip>
-
 using namespace std;
 
-int main() {
-    
-    const float PENSION_RATE = 0.05; 
-    const float TAX_RATE = 0.15;     
+int main()
+{
+    const float penshionRate = 0.05, taxRate = 0.15;
+    float weeklyWorkingHours, bonusRatePerHour, baseSalary, grossSalary, netSalary, bonusPayment, penshion, incomeTax;
     string employeeName;
-    float bonusRate, baseSalary;
-    int weeklyHours;
     
-    cout << "Enter employee name: ";
-    getline(cin, employeeName);
-
-    cout << "Enter weekly working hours: ";
-    cin >> weeklyHours;
-
-    cout << "Enter bonus rate per hour: ";
-    cin >> bonusRate;
-
-    cout << "Enter base salary: ";
-    cin >> baseSalary;
-
-    double grossSalary = baseSalary + (weeklyHours * bonusRate);
-    double bonusPayment = weeklyHours * bonusRate;
-    double pensionDeduction = grossSalary * PENSION_RATE;
-    double taxDeduction = grossSalary * TAX_RATE;
-    double netSalary = grossSalary - pensionDeduction - taxDeduction;
-
-    cout << "\nGross Salary: " << fixed << setprecision(2) << grossSalary << " birr" << endl;
-    cout << "Bonus Payment: " << fixed << setprecision(2) << bonusPayment << " birr" << endl;
-    cout << "Net Salary: " << fixed << setprecision(2) << netSalary << " birr" << endl;
-
-return 0;
-
-
+    cout <<"Enter your name: ";
+     cin>>employeeName;
+    cout <<"Enter your weekly working hours: ";
+     cin>>weeklyWorkingHours;
+    cout<<"bonus rate per hour:";
+     cin>>bonusRatePerHour;
+     cout<<"Enter your base salary: ";
+     cin>>baseSalary;
+    
+   bonusPayment = weeklyWorkingHours * bonusRatePerHour;
+    grossSalary = baseSalary + bonusPayment;
+    
+    penshion = grossSalary * penshionRate;
+    incomeTax = grossSalary * taxRate;
+    netSalary = (grossSalary - penshion -incomeTax);
+    
+    cout<<"Your bonus payment is "<<bonusPayment<<endl;
+    cout<<" Your gross salary is "<<grossSalary<<endl;
+    cout<<"Your net salary is "<<netSalary<<endl;
+    
+    return 0;
+}
+   
+    
+    
+    
 
