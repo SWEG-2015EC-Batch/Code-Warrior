51cd431f21137ab26cbe919cd564f6f115c8f82b
Tewodros Gebretsadik <149199747+Tewodros7@users.noreply.github.com>
2023-11-18 11:40:37 +0300

    Update Question 6.cpp

--- ETS1305_15_TEWODROS_GEBRETSADIK/Question 6.cpp
+++ ETS1305_15_TEWODROS_GEBRETSADIK/Question 6.cpp
@@ -1,20 +1,21 @@
-//calculate the time to send a file and the file size in C++
+//A programthat wil calculate how long it willtake to send a file.
 #include <iostream>
-#include <cmath>
 using namespace std;
-int main() {
-    int transmission_speed = 960; // characters per second
-    int file_size = 419430400; // in bytes
-    double time_taken;
-
-    time_taken = file_size / transmission_speed; // in seconds
-
-    int days = time_taken / (24 * 3600); // convert seconds to days
-    int hours = fmod(time_taken, (24 * 3600)) / 3600; // convert remaining seconds to hours
-    int minutes = fmod(time_taken, 3600) / 60; // convert remaining seconds to minutes
-    int seconds = fmod(time_taken, 60); // remaining seconds
-
-    cout << "Time taken to send the file: " << days << " days, " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds" <<endl;
-
-    return 0;
+int main()
+{
+    const float transmmitionRate =960;
+    int fileSize;
+    float time, hour;
+    
+   
+    cout<<"Entet file size: ";
+    cin>>fileSize;
+    
+    time = ( fileSize / transmmitionRate );
+    
+     hour = time / 3600;
+        
+    cout<<"The time needed to send this file is approximatly "<<time<<" seconds or "<<hour<<" hours."<<endl;
+    
+    return 0;
 }
