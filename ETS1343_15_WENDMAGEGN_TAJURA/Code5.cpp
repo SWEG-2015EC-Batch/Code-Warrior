e2bdf1fe2a8b611648b6ff4e54a4f75a13b5dbb6
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2023-11-23 05:02:05 -0800

    Update Code5.cpp

--- ETS1343_15_WENDMAGEGN_TAJURA/Code5.cpp
+++ ETS1343_15_WENDMAGEGN_TAJURA/Code5.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 int main(){
   
-string employeeName;
+const double penshionRate = 0.05, taxRate = 0.15;
+        string employeeName;
         
         double weeklyHours, baseSalary;
         double overtimeHour, bonusPayment, bonusRate;
@@ -19,23 +20,21 @@ string employeeName;
         cout << "Enter the base salary: \n";
         cin >> baseSalary;
 
-        //calculate over time hours and bonus payment
-        if (hrsWorked > 40){
-            cout <<"Enter worked hours: ";
-            cin >>hrsWorked;
-            overtimeHour = hrsWorked - 40;
-            bonusPayment = bonusRate * overtimeHour;
-        }
+        //calculate bonus payment
+        
+            bonusPayment = bonusRate * weeklyHours;
+        
         //calculate gross salary, net salary, tax rate, and pension
 
         double grossSalary = baseSalary + bonusPayment;
         double pension = 0.05 * grossSalary;
-        double taxRate = 0.15 * grossSalary;
-        double netSalary = grossSalary - (pension + taxRate);
+        double incomeTaxRate = 0.15 * grossSalary;
+        double netSalary = grossSalary - (pension + incomeTaxRate);
         
         //output results
         cout << "Gross Salary: " <<grossSalary <<endl;
         cout << "Bonus Payment: " <<bonusPayment <<endl;
         cout << "Net Salary: " <<netSalary;
+
 return 0;
 }
