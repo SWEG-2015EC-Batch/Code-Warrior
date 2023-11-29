cf9ecc0497451758ac7e51229aa40a159d4a6b68
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2023-11-29 14:03:01 -0800

    Create practiceV3.cpp

---
+++ ETS1343_15_WENDMAGEGN_TAJURA/practiceV3.cpp
@@ -0,0 +1,54 @@
+ //BMI calculator
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+
+    //declare the variable
+    float wight, hight, BMI;
+    char gender;
+    int numOfPerson;
+    //Decide the number of person how many you want to calculate BMI.
+    cout<<"How many person do you want to calculate BMI? n = ";
+    cin>>numOfPerson;
+    for(int i=1; i <= numOfPerson; i++){
+        cout<<"Person "<<i<<" Information is:"<<endl;
+        //Take some data from the user to calculate BMI.
+        cout<<"Please verify your gender (M/F): \n";
+        cin>>gender;
+        cout <<"Please enter the weight: ";
+        cin >>wight;
+        cout <<"Please enter the hight: ";
+        cin >>hight;
+        //BMI calculater function
+        BMI = (wight)/(hight*hight);
+        if(gender == 'M' || gender == 'm'){
+            cout<<"Your gender is Male."<<endl;
+            //After calculate the BMI decide the healthy of body conditions.
+            if(BMI < 20.5){
+                cout<<"Your BMI is: "<<BMI<<" Kg/m^2"<<" you are Under weight. "<<endl;
+            }else  if(BMI >= 20.5 && BMI<=25 ){
+                cout<<"Your BMI is: "<<BMI<<" Kg/m^2"<<" Your are Normal."<<endl;
+            }else if(BMI > 25 && BMI <= 30){
+                cout<<"Your BMI is: "<<BMI<<" Kg/m^2"<<" Your are Over weight."<<endl;
+            }else {
+                cout<<"Your BMI is: "<<BMI<<" Kg/m^2"<<" Your are Obesity."<<endl;
+            }
+        } else if(gender == 'F' || gender == 'f'){
+            cout<<"Your gender is Female."<<endl;
+            if(BMI < 18.5){
+                cout<<"Your BMI is: "<<BMI<<" Kg/m^2"<<" you are Under weight. "<<endl;
+            }else  if(BMI >= 18.5 && BMI<=24 ){
+                cout<<"Your BMI is: "<<BMI<<" Kg/m^2"<<" Your are Normal."<<endl;
+            }else if(BMI > 24 && BMI <= 30){
+                cout<<"Your BMI is: "<<BMI<<" Kg/m^2"<<" Your are Over weight,"<<endl;
+            }else {
+                cout<<"Your BMI is: "<<BMI<<" Kg/m^2"<<" Your are Obesity."<<endl;
+            }
+            //When you enter the wrong information the program remaind you.
+        }else {
+            cout<<"Invalid input, Please Enter The Correct Data.\n";
+        }
+    }
+ return 0;
+}
