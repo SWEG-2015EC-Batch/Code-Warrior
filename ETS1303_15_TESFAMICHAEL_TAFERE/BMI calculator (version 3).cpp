f83ae7b930b909fe700419881870f15476eaa0e0
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-11-30 12:51:44 +0300

    Create BMI calculator (version 3).cpp

---
+++ ETS1303_15_TESFAMICHAEL_TAFERE/BMI calculator (version 3).cpp
@@ -0,0 +1,43 @@
+//BMI calculator
+//By Tesfamichael 
+#include <iostream>
+using namespace std;
+
+int main() {
+    float height, weight, bmi;
+    char gender;
+
+    cout << "Enter your height in meters: ";
+    cin >> height;
+
+    cout << "Enter your weight in kilograms: ";
+    cin >> weight;
+    cout<<"Enter your gender (M or F): ";
+    cin>>gender;
+
+    bmi = weight / (height * height);
+       
+ if(gender=='M'){
+   if(bmi<20.5){
+     cout << "Your Body Mass Index is: " << bmi <<" Kg/m^2"<< ", you are male and under weighted.";
+    }else if(bmi<=20.5 && bmi>25){
+       cout << "Your Body Mass Index is: " << bmi <<" Kg/m^2"<< ", you are male and normal weighted."; 
+    }else if(bmi>=25 && bmi<30){ 
+    cout << "Your Body Mass Index is: " << bmi <<" Kg/m^2"<< ", you are male and over weight.";
+    }else{cout << "Your Body Mass Index is " << bmi <<" Kg/m^2"<< ", you are male and obese.";
+ }
+ }else if(gender=='F'){
+      if(bmi<18.5){
+     cout << "Your Body Mass Index is: " << bmi <<" Kg/m^2"<< ", you are male and under weighted.";
+    }else if(bmi<=18.5 && bmi>24){
+       cout << "Your Body Mass Index is: " << bmi <<" Kg/m^2"<< ", you are male and normal weighted."; 
+    }else if(bmi>=24 && bmi<30){ 
+    cout << "Your Body Mass Index is: " << bmi <<" Kg/m^2"<< ", you are male and over weight.";
+    }else{cout << "Your Body Mass Index is: " << bmi <<" Kg/m^2"<< ", you are male and obese.";
+    }
+ }else{
+    cout<<"Please enter M if you are male or F if you are female.";
+    }
+    return 0;
+
+}
