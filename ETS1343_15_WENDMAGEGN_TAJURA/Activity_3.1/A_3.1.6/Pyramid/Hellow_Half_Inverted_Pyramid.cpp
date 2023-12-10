23bb8060c835b99f299b3f287621ffc862a89fb6
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2023-12-10 11:53:48 -0800

    Create Hellow_Half_Inverted_Pyramid.cpp

---
+++ ETS1343_15_WENDMAGEGN_TAJURA/Activity_3.1/A_3.1.6/Pyramid/Hellow_Half_Inverted_Pyramid.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int i, j, rows;
+    bool stop = false;
+    do{
+    cout << "Enter the number of rows for the Hellow Inverted half pyramid: ";
+    cin >> rows;
+    
+    for(i =rows; i >= 1; i--){
+        for(j = 1; j <= rows - i; j++){
+        }
+        for(j=1; j <= i - 1; j++){
+            if(j==1 || j == i -1 || i == rows){
+                cout<<"* ";
+            }else{
+                cout<<"* ";
+            }
+        }
+        cout<<endl;
+    }
+    cout<<"Entet 0 to exit Or enter 1 to countinue: ";
+    cin>>stop;
+    }while(stop != 0);
+    
+    return 0;
+}
