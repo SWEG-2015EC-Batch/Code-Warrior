85ada75471eaa03d85cd63a6cd7ce63c217ac9db
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2023-12-10 12:00:27 -0800

    Create abcPattern.cpp

---
+++ ETS1343_15_WENDMAGEGN_TAJURA/Activity_3.1/A_3.1.6/Pattern/abcPattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char i, j;
+    
+    bool stop = false;
+    
+    do{
+    cout << "The number of Pattern is look like: \n\n";
+
+    for ( i = 'a'; i <= 'e'; i++) {
+        for ( j = 'a'; j <= 'e'; j++) {
+            cout <<j<< " ";
+        }
+         
+        cout << endl;
+    }
+    cout<<"\nEnter 0 to exit or Enter 1 to continue: ";
+    cin>>stop;
+    }while(stop != 0);
+
+    return 0;	
+}
