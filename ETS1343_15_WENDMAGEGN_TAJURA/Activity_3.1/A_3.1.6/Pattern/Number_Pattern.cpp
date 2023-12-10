27987c5a5c8d2130d7ae96f38b244a765a488cfb
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2023-12-10 12:04:08 -0800

    Create Number_Pattern.cpp

---
+++ ETS1343_15_WENDMAGEGN_TAJURA/Activity_3.1/A_3.1.6/Pattern/Number_Pattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char i, j;
+    
+    bool stop = false;
+    
+    do{
+    cout << "The number of Pattern is look like: \n\n";
+
+    for ( i = 1; i <= 5; i++) {
+        for ( j = 1; j <= 5; j++) {
+            cout <<j<< " ";
+        }
+         
+        cout << endl;
+    }
+    cout<<"\nEnter 0 to exit or Enter 1 to continue: ";
+    cin>>stop;
+    }while(stop != 0);
+
+    return 0;	
+}
