7f1b89abb06956b22d5623dfe53fe775d3af2fe2
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-12-11 18:21:38 +0300

    Create Code V1.cpp

---
+++ ETS1303_15_TESFAMICHAEL_TAFERE/Activity 3.1/Grade calcutor/Code V1.cpp
@@ -0,0 +1,33 @@
+//Grade calculator 
+//By Tesfamichael 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score;
+    
+    cout<<"Enter your score: ";
+    cin>>score;
+    
+     if(score >=90 && score <= 100)
+    cout<<"A+";
+    else if(score <90 && score >=80)
+    cout<<"A";
+    else if(score <80 && score >=75)
+    cout<<"B+";
+    else if(score <75 && score >=60)
+    cout<<"B";
+    else if (score <60 && score>=55)
+    cout<<"C+";
+    else if(score <55 && score >=45)
+    cout<<"C";
+    else if(score <45 && score >=35)
+    cout<<"D";
+    else if (score <0 || score >100)
+    cout<<"Enter a valid score.";
+    else
+    cout<<"F";
+
+  return 0;  
+}
