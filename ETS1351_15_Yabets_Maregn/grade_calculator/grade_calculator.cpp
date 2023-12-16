4659f19a0dc741c025f869bde7d1a9ae1438fd9d
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2023-12-16 05:45:49 -0500

    grade_calculator.cpp

--- ETS1351_15_Yabets_Maregn/grade_calculator/grade_calculator.cpp
+++ ETS1351_15_Yabets_Maregn/grade_calculator/grade_calculator.cpp
@@ -1,1 +1,45 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    float mark;
+    cout<<"Enter Mark:- ";
+    cin>>mark;
+    if (mark<=100 && mark>=90)
+    {
+        cout<<"A+";
+    }
+    else if (mark<90 && mark>=80)
+    {
+        cout<<"A";
+    }
+    else if (mark<80 && mark>=75)
+    {
+        cout<<"B+";
+    }
+    else if (mark<75 && mark>=60)
+    {
+        cout<<"B";
+    }
+    else if (mark<60 && mark>=55)
+    {
+        cout<<"C+";
+    }
+    else if (mark<55 && mark>=45)
+    {
+        cout<<"C";
+    }
+    else if (mark<45 && mark>=30)
+    {
+        cout<<"D";
+    }
+    else if (mark<30 && mark>=0)
+    {
+        cout<<"F";
+    }
+    else  
+    {
+        cout<<"sorry you doesn't entered the appropriate mark, mark should be in the range of [0-100]";
+    }
+    
+    return 0; 
+}
