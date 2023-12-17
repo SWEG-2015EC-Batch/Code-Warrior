557af370d64eabd4d72c653ae32a8c28b2c3c2f6
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2023-12-17 10:38:39 -0500

    Update bmi_version_v3.cpp

--- ETS1351_15_Yabets_Maregn/bmi_calculator/bmi_version_v3.cpp
+++ ETS1351_15_Yabets_Maregn/bmi_calculator/bmi_version_v3.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+int main(){
+     //in this code bmi stands for body mass index
+    float weight, height, bmi;
+   int i, num;
+    cout<<"for how many person do you want to calculate:- ";
+    cin>>num;
+    
+    //bmi can be calculted by dividing the weight in kilogram by height squred in meter. 
+    
+    for ( i = 1; i <=num ; i++)
+    {
+        cout<<"enter weight in kilogram:- ";
+    cin>>weight;
+    cout<<"enter height in meter:- ";
+    cin>>height;
+    
+    
+        bmi = weight/(height*height);
+        cout<<"BMI = "<<bmi<<endl;
+    }
+    
+    return 0;
+}
