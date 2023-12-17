87a67819c21d3a853e98c9e50a49ea6436495ecb
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-12-17 23:25:00 +0300

    Create Pyramid.cpp

---
+++ ETS1303_15_TESFAMICHAEL_TAFERE/Activity 3.1/Printing patterns/Pyramid.cpp
@@ -0,0 +1,20 @@
+//Printing a pyramid 
+//By Tesfamichael 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int I, j, n=5;
+    
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n - i; ++j) {
+            cout << "  "; // Print spaces for indentation
+        }
+        for (int j = 1; j <= 2 * i - 1; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+}
+return 0;
+}
