801f07527e242e2f27edae221135817ed0d23ddb
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-12-17 22:58:59 +0300

    Create Square.cpp

---
+++ ETS1303_15_TESFAMICHAEL_TAFERE/Activity 3.1/Printing patterns/Square.cpp
@@ -0,0 +1,17 @@
+//Printing square
+//By Tesfamichael 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int j, i, n=5;
+    
+    for(i=1; i<=n; ++i){
+    for(j=1; j<=n; ++j){
+        cout<<"* ";
+    }
+    cout<<endl;
+    }
+    return 0;
+}
