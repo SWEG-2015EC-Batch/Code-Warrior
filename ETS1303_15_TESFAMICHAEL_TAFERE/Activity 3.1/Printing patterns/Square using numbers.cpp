611e4dc07ccc7f17e0c48492c65e51ec078c7688
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-12-18 02:18:31 +0300

    Create Square using numbers.cpp

---
+++ ETS1303_15_TESFAMICHAEL_TAFERE/Activity 3.1/Printing patterns/Square using numbers.cpp
@@ -0,0 +1,17 @@
+//Printing a square using numbers 
+//By Tesfamichael 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5; 
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
