433c158e92e9c03fcc28404b30f1e2cf21de6a6e
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2023-12-18 02:24:25 +0300

    Create Half of a triangle using numbers.cpp

---
+++ ETS1303_15_TESFAMICHAEL_TAFERE/Activity 3.1/Printing patterns/Half of a triangle using numbers.cpp
@@ -0,0 +1,17 @@
+//Printing half of a triangle using numbers 
+//By Tesfamichael 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height = 5;  
+    
+    for (int i = 1; i <= height; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
