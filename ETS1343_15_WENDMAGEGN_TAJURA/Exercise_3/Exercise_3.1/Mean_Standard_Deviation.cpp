a2a566d7ce9b12f5fc103f4a669265e408eb6961
Wendmagegn Tajura <149209819+wende12github@users.noreply.github.com>
2023-12-19 14:35:25 +0300

    Create Mean_Standard_Deviation.cpp
    This for Mean and Standard Deviation program

---
+++ ETS1343_15_WENDMAGEGN_TAJURA/Exercise_3/Exercise_3.1/Mean_Standard_Deviation.cpp
@@ -0,0 +1,37 @@
+//MEAN AND STANDARD DEVIATION CALCULATIONS 
+//Programmed by WENDMAGEGN TAJURA
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n;
+    int total = 0;
+    float mean = 0, variance = 0, standardDeviation = 0, sum = 0, sumOfSquares = 0;
+            
+    cout << "How many data entries will you? n: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int mark, frequency;
+        
+        cout << "Enter the data: ";
+        cin >> mark;
+
+        cout << "Enter the data frequency: ";
+        cin >> frequency;
+
+        sum += mark * frequency;
+        sumOfSquares += frequency*pow(mark - mean, 2);
+        total += frequency;
+    }
+
+    mean = sum / total;
+    variance = sumOfSquares / total;
+    standardDeviation = sqrt(variance);
+   
+    cout << "The mean is: " << mean << endl;
+    cout << "The Variance is: " << variance << endl;
+    cout << "The Standard Deviation is: " << standardDeviation << endl;
+
+    return 0;
+}
