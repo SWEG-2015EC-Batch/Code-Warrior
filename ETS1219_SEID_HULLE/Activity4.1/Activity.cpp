28951f5ea3f011ec9ebec55eda5b988220ec7ec4
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2023-12-19 21:49:27 +0300

    Create Activity.cpp

---
+++ ETS1219_SEID_HULLE/Activity4.1/Activity.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    float num, sum = 0, ave, SD, VR =0;
+
+    vector<float> numbers;
+
+    cout << "How many numbers do you want to work with: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) 
+    {
+        cout << "Enter a number: ";
+        cin >> num;
+        numbers.push_back(num);
+        sum += num;
+    }
+    ave = sum / n;
+    for (float num : numbers) {
+        VR += pow(num - ave, 2);
+    }
+    SD = sqrt(VR / n);
+    cout << "The entered numbers: ";
+    for (float num : numbers) {
+        cout << num << " ";
+    }
+
+    cout << "\nThe mean for the entered numbers is: " << ave << "\n";
+    cout << "The standard deviation is: " << SD;
+
+    return 0;
+}
