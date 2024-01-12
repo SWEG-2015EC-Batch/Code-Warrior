204445f32ea102f9335b6f747fd2ebd518b22130
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-12 10:46:27 +0300

    Update Q#2.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#2.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#2.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    const int SIZE = 8;
+    int temp[SIZE];
+    int total = 0;
+    
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Enter number " << i + 1 << ": ";
+        cin >> temp[i];
+        total += temp[i];
+    }
+
+    double average = static_cast<double>(total) / SIZE;
+    cout << "Sum: " << total <<endl;
+    cout << "Average: " << average <<endl;
+  return 0;
+}
