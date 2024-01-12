62e5ba48289cdd25570d2d52de9e56bf37d39351
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-12 10:47:17 +0300

    Update Q#4.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#4.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#4.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+using namespace std;
+const int ROWS = 3;
+const int COLS = 4;
 
+int main() {
+    int first[ROWS][COLS];
+    int second[ROWS][COLS];
+    int result[ROWS][COLS];
+    cout << "Enter elements for the first array:" << endl;
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            cout << "Enter element at position (" << i << ", " << j << "): ";
+            cin >> first[i][j];
+        }
+    }
+    cout << "Enter elements for the second array:" << endl;
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+           cout << "Enter element at position (" << i << ", " << j << "): ";
+           cin >> second[i][j];
+        }
+    }
+    
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            result[i][j] = first[i][j] + second[i][j];
+        }
+    }
+    cout << "Resulting data elements:" <<endl;
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            cout << result[i][j] << "\t";
+        }
+           cout <<endl;
+    }
+
+    return 0;
+}
