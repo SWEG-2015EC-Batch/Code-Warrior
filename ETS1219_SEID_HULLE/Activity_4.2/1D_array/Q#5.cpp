12a7aeb74490fe4ecf21b3d999a46c23596153c5
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-12 10:48:09 +0300

    Update Q#5.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#5.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#5.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+const int SIZE = 9;
+const int COLUMNS = 3;
 
+int main() {
+    double volts[SIZE] = {11.95, 16.32, 12.15, 8.22, 15.98, 26.22, 13.54, 6.45, 17.59};
+    for (int i = 0; i < SIZE; i++) {
+        cout << volts[i] << "\t";
+    if ((i + 1) % COLUMNS == 0) {
+         cout <<endl;
+        }
+    }
+
+    return 0;
+}
