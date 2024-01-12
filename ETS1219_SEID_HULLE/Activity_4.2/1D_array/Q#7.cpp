c90417705e271a3ad8b056db3e86c465498540b0
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-12 10:49:47 +0300

    Update Q#7.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#7.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#7.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> numbers;
 
+    int num;
+    cout << "Enter numbers (enter 0 to stop):" << endl;
+    while (true) {
+        cin >> num;
+        if (num == 0) {
+            break;
+        }
+        numbers.push_back(num);
+    }
+
+      cout << "Even numbers in reverse order: ";
+    for (int i = numbers.size() - 1; i >= 0; i--) {
+        if (numbers[i] % 2 == 0) {
+            cout << numbers[i] << " ";
+        }
+    }
+    cout << std::endl;
+
+    return 0;
+}
