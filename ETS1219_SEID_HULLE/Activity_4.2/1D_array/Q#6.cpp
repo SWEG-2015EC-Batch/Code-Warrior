917ec4d535aa5045898c62e8241ef8e899a0d315
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-12 10:48:59 +0300

    Update Q#6.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#6.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/1D_array/Q#6.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+    int totalMarks;
+    cout << "Enter the total marks: ";
+    cin >> totalMarks;
+    string scoreLevel;
+    if (totalMarks >= 80) {
+        scoreLevel = "Excellent";
+    } else if (totalMarks >= 60) {
+        scoreLevel = "Very Good";
+    } else if (totalMarks >= 50) {
+        scoreLevel = "Fair";
+    } else if (totalMarks >= 40) {
+        scoreLevel = "Poor";
+    } else {
+        scoreLevel = "Fail";
+    }
+    cout << left << setw(15) << "Score Level"
+              << setw(10) << "Total Marks" << endl;
+    cout << left << setw(15) << scoreLevel
+              << setw(10) << totalMarks << endl;
+
+    return 0;
 
