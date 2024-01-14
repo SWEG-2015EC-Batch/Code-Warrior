fadfe3d973584a807e5b2d7b6d1be34a91b2f0e8
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:55:37 +0300

    Update Q#1.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/B2D_array/Q#1.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/B2D_array/Q#1.cpp
@@ -1,1 +1,42 @@
 
+#include <iostream>
+using namespace std;
+
+bool areArraysIdentical(const int arr1[], const int arr2[], int size) {
+    for (int i = 0; i < size; ++i) {
+        if (arr1[i] != arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    const int maxSize = 100; 
+    int size;
+    cout << "Enter the size of the arrays: ";
+    cin >> size;
+
+    int array1[maxSize];
+    cout << "Enter elements for the first array:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << "Element " << (i + 1) << ": ";
+        cin >> array1[i];
+    }
+
+    int array2[maxSize];
+    cout << "Enter elements for the second array:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << "Element " << (i + 1) << ": ";
+        cin >> array2[i];
+    }
+
+
+    if (areArraysIdentical(array1, array2, size)) {
+        cout << "\nThe arrays are identical." << endl;
+    } else {
+        cout << "\nThe arrays are not identical." << endl;
+    }
+
+    return 0;
+}
