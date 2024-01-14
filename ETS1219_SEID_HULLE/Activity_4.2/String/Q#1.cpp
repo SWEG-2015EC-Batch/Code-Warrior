82e21df27da0d84229da2f30623cad3167448ac1
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:14:57 +0300

    Update Q#1.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/String/Q#1.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/String/Q#1.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string inputString;
+    
+    cout << "Enter a string: ";
+    getline(cin, inputString);
+
+    int vowelCount = 0, consonantCount = 0;
+    for (char c : inputString) {
+        if (isalpha(c)) {
+            char lowerC = tolower(c);
+            if (lowerC == 'a' || lowerC == 'e' || lowerC == 'i' || lowerC == 'o' || lowerC == 'u') {
+                vowelCount++;
+            } else {
+                consonantCount++;
+            }
+        }
+    }
+
+    cout << "\nNumber of vowels: " << vowelCount << endl;
+    cout << "Number of consonants: " << consonantCount << endl;
+
+    return 0;
+}
 
