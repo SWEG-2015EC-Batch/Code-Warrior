8869b13647fa20e22df3968754fd9a205806f605
Tewodros Gebretsadik <149199747+Tewodros7@users.noreply.github.com>
2024-01-14 22:46:09 +0300

    Create Removing non-alphabet characters .cpp

---
+++ ETS1305_15_TEWODROS_GEBRETSADIK/Activity 4.2/String/Removing non-alphabet characters .cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    string result;
+
+    for (char ch : input) {
+        if (isalpha(ch)) {
+            result += ch;
+        }
+    }
+
+    cout << "String after removing non-alphabet characters: " << result << "\n";
+
+    return 0;
+}
