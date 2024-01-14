0b919c5cf93c7e2d6691bb619679035f807068dc
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:23:26 +0300

    Update Q#11.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/String/Q#11.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/String/Q#11.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string firstName, middleName, lastName;
+
+    cout << "Enter your first name: ";
+    cin >> firstName;
+
+    cout << "Enter your middle name: ";
+    cin >> middleName;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+    cout << "\nYour initials are: " << firstName[0] << middleName[0] << lastName[0] << endl;
+
+    return 0;
+}
 
