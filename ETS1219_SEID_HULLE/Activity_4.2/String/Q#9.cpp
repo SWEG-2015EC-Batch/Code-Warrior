ffb087ef14cef376327fef4f3d5f6ae74f2649d1
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:22:10 +0300

    Update Q#9.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/String/Q#9.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/String/Q#9.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <cstring>
 
+using namespace std;
+
+int main() {
+  
+    char firstProgram[] = "Breaking Bad";
+    char secondProgram[50]; 
+    strcpy(secondProgram, "Stranger Things");
+
+    cout << "My two favorite television programs are:" << endl;
+    cout << "1. " << firstProgram << endl;
+    cout << "2. " << secondProgram << endl;
+
+    return 0;
+}
