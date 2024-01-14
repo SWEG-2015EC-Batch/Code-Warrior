f6d1b1be3d15986857203bb6d898a57e770ba975
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:21:17 +0300

    Update Q#8.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/String/Q#8.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/String/Q#8.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    const int MAX_LENGTH = 100; 
+    char inputText[MAX_LENGTH];
+
+    cout << "Enter a line of text: ";
+    cin.getline(inputText, MAX_LENGTH);
+    cout << "Uppercase: ";
+    for (int i = 0; i < strlen(inputText); ++i) {
+        cout << static_cast<char>(toupper(inputText[i]));
+    }
+    cout << endl;
+    cout << "Lowercase: ";
+    for (int i = 0; i < strlen(inputText); ++i) {
+        cout << static_cast<char>(tolower(inputText[i]));
+    }
+    cout << endl;
+
+    return 0;
+}
 
