f2acf57a83dedaf3287cf7a127043bd35a3b8846
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:17:04 +0300

    Update Q#3.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/String/Q#3.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/String/Q#3.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string inputString;
+    cout << "Enter a string: ";
+    getline(cin, inputString);
+
+    string resultString = "";
+    for (char c : inputString) {
+        if (isalpha(c)) {
+            resultString += c;
+        }
+    }
+
+    cout << "\nString after removing non-alphabet characters: " << resultString << endl;
+
+    return 0;
+}
 
