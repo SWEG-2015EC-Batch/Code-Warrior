3b433367f8808f9086abdb59b15d139da905ec91
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:22:48 +0300

    Update Q#10.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/String/Q#10.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/String/Q#10.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    const char correctPassword[] = "secure123"; /
+    char userInput[50];
+
+    cout << "Enter the password: ";
+    cin >> userInput;
+
+    if (strcmp(userInput, correctPassword) == 0) {
+        cout << "Correct password. Access granted." << endl;
+    } else {
+        cout << "Incorrect password. Access denied." << endl;
+    }
+
+    return 0;
+}
 
