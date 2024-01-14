353550e6c743a2c8c6fcace48a0b05608187f9be
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:18:12 +0300

    Update Q#4.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/String/Q#4.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/String/Q#4.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string inputText;
+
+    cout << "Enter a line of text: ";
+    getline(cin, inputText);
+
+    stringstream ss(inputText);
+    vector<string> tokens;
+    string token;
+
+    while (ss >> token) {
+        tokens.push_back(token);
+    }
+
+    cout << "\nTokens (separated by spaces):" << endl;
+    for (const string& t : tokens) {
+        cout << t << endl;
+    }
+
+    return 0;
+}
 
