a5f11acfc9dd18ee37e1d461ff732bfd95ce2099
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:15:57 +0300

    Update Q#2.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/String/Q#2.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/String/Q#2.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string word;
+
+    cout << "Enter a word: ";
+    cin >> word;
+    transform(word.begin(), word.end(), word.begin(), ::tolower);
+
+    bool isPalindrome = true;
+    int length = word.length();
+
+    for (int i = 0; i < length / 2; ++i) {
+        if (word[i] != word[length - 1 - i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if (isPalindrome) {
+        cout << "\nThe word is a palindrome." << endl;
+    } else {
+        cout << "\nThe word is not a palindrome." << endl;
+    }
+
+    return 0;
+}
 
