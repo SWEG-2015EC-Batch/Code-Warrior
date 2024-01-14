4f8d8532a05e454241e970b703821c6bd11e76d6
Seidhulle19 <149239831+Seidhulle@users.noreply.github.com>
2024-01-14 21:20:36 +0300

    Update Q#7.cpp

--- ETS1219_SEID_HULLE/Activity_4.2/String/Q#7.cpp
+++ ETS1219_SEID_HULLE/Activity_4.2/String/Q#7.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<string> names;
+    int numNames;
+
+    cout << "Enter the number of names: ";
+    cin >> numNames;
+    cout << "Enter the names, one by one:" << endl;
+    for (int i = 0; i < numNames; ++i) {
+        string name;
+        cout << "Name " << (i + 1) << ": ";
+        cin >> name;
+        names.push_back(name);
+    }
+
+    sort(names.begin(), names.end());
+    cout << "\nNames in alphabetical order:" << endl;
+    for (const string& name : names) {
+        cout << name << endl;
+    }
+
+    return 0;
+}
 
